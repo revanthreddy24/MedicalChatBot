2a2e227f27a7ced86e2b0b5e6034b2022745b0de
revanthreddy24 <revanthme24@gmail.com>
2025-08-24 16:15:11 -0400

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="Generative AI projects",
+    version="0.0.0",
+    author="revanth",
+    author_email="revanthme24@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
